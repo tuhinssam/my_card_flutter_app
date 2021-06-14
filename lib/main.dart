9f7fb115fd6f3d68ff27f948a1f2b11c624fa2e4
tuhinssam <tuhinssam@gmail.com>
2021-06-14 16:58:00 +0530

    all widgets added for business card

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.lightBlue[700],
         body: SafeArea(
             child: Column(
           //mainAxisSize: MainAxisSize.min,
           //verticalDirection: VerticalDirection.down,
-          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.center,
           //crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             CircleAvatar(
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
               backgroundImage: AssetImage('images/profile.gif'),
             ),
             Text(
-              'Mr Dexter',
+              'Tuhin S Samanta',
               style: TextStyle(
                   fontFamily: 'Pacifico',
                   fontSize: 30,
@@ -39,8 +39,63 @@ class MyApp extends StatelessWidget {
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.0,
-                  color: Colors.white),
-            )
+                  color: Colors.white54),
+            ),
+            SizedBox(
+              height: 20,
+              width: 150,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    '+91-9830305950',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'tuhinssam@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                    ),
+                  )
+                ],
+              ),
+            ),
 //             Container(
 //               height: 100,
 //               //width: double.infinity,
